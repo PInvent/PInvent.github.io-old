1c0e2daa8b3713619328a2a461f1a763194d0a5b
Azman <azman95ict@gmail.com>
2019-03-15 16:07:53 +0800

    Updates

---
+++ precache-manifest.33cc07e664391f1cc90481a1e4c0f298.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.c5541365.js"
+  },
+  {
+    "revision": "33c51d707cf7816234da",
+    "url": "/static/js/main.627e82c8.chunk.js"
+  },
+  {
+    "revision": "a539ea9d3b1acab9c730",
+    "url": "/static/js/2.948d4d77.chunk.js"
+  },
+  {
+    "revision": "33c51d707cf7816234da",
+    "url": "/static/css/main.6f1d2343.chunk.css"
+  },
+  {
+    "revision": "c41dbdbe74703b3ccf8463209137557d",
+    "url": "/index.html"
+  }
+];
