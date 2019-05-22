751dfb9661d3e6d9e15f59c3ebce0e7a38a17021
Azman <azman95ict@gmail.com>
2019-05-22 22:44:26 +0800

    Service card img placeholder

--- src/Onepager/Services.js
+++ src/Onepager/Services.js
@@ -2,7 +2,11 @@ import React, { Component } from "react";
 import "./../App.css";
 import { Row, Col, Card } from "react-bootstrap";
 import { TEXT_SERVICES } from "../Util/Text";
-import img from "../img/pinvent-logo-wide.jpeg";
+// import img from "../img/pinvent-logo-wide.jpeg";
+import consultation from "../img/Services/consultation.png";
+import classes from "../img/Services/classes.png";
+import outsourcing from "../img/Services/outsourcing.png";
+import product from "../img/Services/product.png";
 
 // let serv = [
 //   {
@@ -26,10 +30,10 @@ import img from "../img/pinvent-logo-wide.jpeg";
 let Cardsss = val => (
   <div className="card-container">
     <Card>
-      <Card.Img variant="top" src={img} />
+      <Card.Img variant="top" src={val.img} />
       <Card.Body>
         <Card.Title>{val.title}</Card.Title>
-        {/* <Card.Text>lorem ipsum de amet</Card.Text> */}
+        <Card.Text>lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam id urna vehicula</Card.Text>
       </Card.Body>
     </Card>
   </div>
@@ -44,12 +48,12 @@ class Services extends Component {
 
           <Row>
             <Col lg={6} md={12}>
-              <Cardsss title="Consultation" />
-              <Cardsss title="Outsourcing" />
+              <Cardsss title="Consultation" img={consultation} />
+              <Cardsss title="Outsourcing" img={outsourcing}  />
             </Col>
             <Col lg={6} md={12}>
-              <Cardsss title="Class" />
-              <Cardsss title="Product" />
+              <Cardsss title="Class" img={classes}  />
+              <Cardsss title="Product" img={product}  />
             </Col>
           </Row>
         </div>
